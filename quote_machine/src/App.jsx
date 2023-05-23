ddebc8c4f701dd66fd1a50da37f6b10856e27777
JoseMiguelVazquez <jmiguel_1991@hotmail.com>
2023-05-23 14:19:53 -0600

    random color

--- quote_machine/src/App.jsx
+++ quote_machine/src/App.jsx
@@ -7,6 +7,27 @@ function App () {
   const [quote, setQuote] = useState({})
   const [color, setColor] = useState('')
 
+  const colorPallete = [
+    '#0A4D68',
+    '#088395',
+    '#05BFDB',
+    '#00FFCA',
+    '#068DA9',
+    '#7E1717',
+    '#E55807',
+    '#4C4C6D',
+    '#1B9C85',
+    '#1F8A70',
+    '#39B5E0',
+    '#9C254D'
+  ]
+
+  const getRandomColor = (colorArray) => {
+    const randomIndex = Math.floor(Math.random() * colorArray.length)
+    const randomColor = colorArray[randomIndex]
+    return randomColor
+  }
+
   const getNewQuote = () => {
     axios.get(API_URL)
       .then(response => {
@@ -15,11 +36,12 @@ function App () {
       .catch(error => {
         console.log(error)
       })
+    setColor(getRandomColor(colorPallete))
   }
 
   useEffect(() => {
     getNewQuote()
-    setColor('aqua')
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   return (
@@ -28,15 +50,25 @@ function App () {
         className='d-flex flex-column align-items-center justify-content-center h-100'
         style={{ backgroundColor: color }}
       >
-        <div id='quote-box' className='container p-5'>
-          <p id='text'>{quote.content}</p>
+        <div id='quote-box' className='container p-5 mb-2'>
+          <p
+            id='text'
+            style={{ color }}
+          >{quote.content}
+          </p>
           <div className='d-flex justify-content-end'>
-            <p id='author'>- {quote.author}</p>
+            <p
+              id='author'
+              style={{ color }}
+            >- {quote.author}
+            </p>
           </div>
           <div className='d-flex justify-content-between mt-4'>
             <button
               id='new-quote'
+              className='btn'
               onClick={getNewQuote}
+              style={{ backgroundColor: color, color: 'white' }}
             >New Quote
             </button>
             <a
@@ -48,7 +80,16 @@ function App () {
             </a>
           </div>
         </div>
-        <p>Quotes provided by <a href='https://github.com/lukePeavey/quotable' target='_blank' rel='noreferrer'>Quotable API</a></p>
+        <p className='quotes-provided-by'>Quotes provided by
+          <span> </span>
+          <a
+            href='https://github.com/lukePeavey/quotable'
+            target='_blank'
+            rel='noreferrer'
+          >
+            Quotable API
+          </a>
+        </p>
       </div>
     </>
   )
